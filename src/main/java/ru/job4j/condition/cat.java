f57bbcd1ce95ffd8e60e7d33402a939b5442e629
lizot <lizoto@yandex.ru>
2024-10-12 00:45:03 +0300

    9.2. Преобразование примитивных типов [#383106]

---
+++ src/main/java/ru/job4j/condition/cat.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+public class cat {
+    public static void main(String[] args) {
+        long l = 129;
+        byte b = (byte) l;
+        System.out.println(b);
+
+        float f = 12358.7f;
+        int i = 45981;
+        short result = (short) (f + i);
+        System.out.println(result);
+
+        char c = 45000;
+        float o = c;
+        System.out.println(o);
+
+        double d = 121.19;
+        byte k = (byte) d;
+        System.out.println(k);
+
+        short e = 1500;
+        char n = (char) e;
+        double w = c;
+        System.out.println(w);
+}
+}
