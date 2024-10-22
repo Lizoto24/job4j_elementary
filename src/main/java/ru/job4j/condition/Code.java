c7e1e8ec99b371ca4cd64745bad65e611e968bd2
lizot <lizoto@yandex.ru>
2024-10-22 23:37:43 +0300

    4.1.4. Псевдокод [#313436 #35403]

---
+++ src/main/java/ru/job4j/condition/Code.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class Code {
+    public static int number(int A, int B, int C) {
+        if (A > B) {
+            if (A > C) {
+                return (A);
+            } else {
+                return (C);
+            }
+        } else if (B > C) {
+            return (B);
+        } else {
+            return (C);
+        }
+    }
+    public static void main(String[] args) {
+       int result = Code.number (20,90,48);
+       System.out.println("Самое большое числ "   +result);
+    }
+}
+
