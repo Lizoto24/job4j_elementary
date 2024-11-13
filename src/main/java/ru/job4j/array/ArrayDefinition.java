fe95b133baed00dd8560946d02d1330f660b1b08
lizot <lizoto@yandex.ru>
2024-11-13 14:01:01 +0300

    6.0.3. Заполнение массива. [#226300 #35415]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String [] names = new String [4];
+        names [0] = "Елизавета";
+        names [1] = "Алексей";
+        names [2] = "Бублик";
+        names [3] = "Чебурахтор";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
