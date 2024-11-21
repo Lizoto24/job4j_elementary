6839060c774f5d51869489bf22e8c0c50d0c03d4
lizot <lizoto@yandex.ru>
2024-11-21 14:46:21 +0300

    6.5. Поиск индекса в диапазоне. [#156318 #35419]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class FindLoop {
     public static int indexOf(int[] data, int element) {
         int result = -1;
-        for (int index = 0; index < data.length ; index++) {
+        for (int index = 0; index < data.length; index++) {
             if (data[index] == element) {
                 result = index;
                 break;
@@ -12,12 +12,32 @@ public class FindLoop {
         return result;
     }
 
-    public static void main(String[] args) {
-        int [] data = new int [] {5,10,15};
-        int element = 5;
-        int result = FindLoop. indexOf (data,element);
-        System.out.println(result);
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == element) {
+                result = index;
+                break;
+            }
+
+        }
+        return result;
     }
+
+    public static void main(String[] args) {
+        int[] data = new int[]{5, 10, 15};
+        int element = 5;
+        int result = FindLoop.indexOf(data, element);
+        System.out.println(result);
+
+        int[] data1 = new int[]{6, 7, 9, 17, 35, 89, 92, 64, 78, 92, 47};
+        int element1 = 92;
+        int start = 3;
+        int finish = 9;
+        int result1 = FindLoop.indexInRange(data1, element1, start, finish);
+        System.out.println(result1);
+    }
+
 }
 
 
