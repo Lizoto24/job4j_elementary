0119805fbbc556ea59b9f2d9bf7f8636f8513903
lizot <lizoto@yandex.ru>
2024-11-26 02:42:29 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859 #35427]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,54 +5,76 @@ import java.util.Arrays;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = 0; i < board [row].length; i++) {
-            if (board [row][i] != 'X') {
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 'X') {
                 result = false;
                 break;
             }
         }
         return result;
     }
+
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (int row = 0; row < board.length; row++) {
-            if (board [row][column] != 'X') {
+            if (board[row][column] != 'X') {
                 result = false;
                 break;
             }
         }
         return result;
     }
+
     public static char[] extractDiagonal(char[][] board) {
         char[] result = new char[board.length];
         for (int row = 0; row < board.length; row++) {
-            result [row] = board [row][row];
+            result[row] = board[row][row];
+        }
+        return result;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (MatrixCheck.monoHorizontal(board, i) || MatrixCheck.monoVertical(board, i)) {
+                result = true;
+                break;
+            }
         }
         return result;
     }
 
     public static void main(String[] args) {
         int row = 2;
-        char [][] board = new char [][] { {' ', ' ', ' '},
+        char[][] board = new char[][]{{' ', ' ', ' '},
                 {'X', 'X', 'X'},
                 {' ', ' ', ' '}};
-        boolean result = MatrixCheck.monoHorizontal(board,2);
+        boolean result = MatrixCheck.monoHorizontal(board, 2);
         System.out.println(result);
 
         int column = 2;
-        char[][] board1 =  new char [][]{
+        char[][] board1 = new char[][]{
                 {' ', ' ', 'X'},
                 {' ', ' ', 'X'},
                 {' ', ' ', 'X'}};
-        boolean result1 = MatrixCheck.monoVertical(board1,2);
+        boolean result1 = MatrixCheck.monoVertical(board1, 2);
         System.out.println(result1);
 
-        char[][] board2 =  new char [][]{
+        char[][] board2 = new char[][]{
                 {'X', ' ', ' '},
                 {' ', 'X', ' '},
                 {' ', ' ', 'X'}};
-        char [] result2 = MatrixCheck.extractDiagonal(board2);
+        char[] result2 = MatrixCheck.extractDiagonal(board2);
         System.out.println(result2);
 
+
+        char[][] boar3 = new char[][]{{' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '}};
+        boolean result3 =MatrixCheck .isWin(board);
+        System.out.println(result1);
     }
 }
+
--- src/test/java/ru/job4j/array/MatrixTest.java
+++ src/test/java/ru/job4j/array/MatrixTest.java
@@ -28,4 +28,55 @@ public class MatrixTest {
         };
         assertThat(result).isDeepEqualTo(expected);
     }
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        char[][] input = {
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenDataHMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenDataDiagMonoIsTrueThenFalse() {
+        char[][] input = {
+                {'X', ' ', ' ', ' ', ' '},
+                {' ', 'X', ' ', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', ' ', 'X', ' '},
+                {' ', ' ', ' ', ' ', 'X'},
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isFalse();
+    }
 }
